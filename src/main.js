b76a84c75403a268159e47d2228a867c1b56eaa4
blackst0ne <blackst0ne.ru@gmail.com>
2017-09-13 14:51:21 +1100

    Add twitter bootstrap v4

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
