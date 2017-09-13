49e7a9ef997d83d0b8ed6cc99947234a704218ca
blackst0ne <blackst0ne.ru@gmail.com>
2017-09-13 14:14:05 +1100

    Add vuejs

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer id="footer">
+  	I am footer
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    <header id="header">
+    	I am header
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+
