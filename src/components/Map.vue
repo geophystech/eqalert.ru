64269af7d4a741a03d936331ae479a1dadc5f5ef
blackst0ne <blackst0ne.ru@gmail.com>
2017-09-20 10:56:57 +1100

    Fix header

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="map">
   	THIS IS <del>SPARTA</del> MAP!
+    <vue-markdown>
+  	  I am a ~~tast~~ **test**.
+    </vue-markdown>
   </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 export default {
-  name: 'map'
+  name: 'map',
+  components: { VueMarkdown }
 }
 </script>
 
