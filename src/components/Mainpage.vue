70294d913bc8b0db0ca8ec2428cbed0110a01c27
blackst0ne <blackst0ne.ru@gmail.com>
2017-09-19 16:11:15 +1100

    Add Map component

--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="mainpage">
+    <Map />
   </div>
 </template>
 
 <script>
+import Map from '@/components/Map'
+
+export default {
+  name: 'mainpage',
+  components: { Map }
+}
 </script>
 
 <style scoped>
---
+++ src/components/Map.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="map">
+  	THIS IS <del>SPARTA</del> MAP!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'map'
+}
+</script>
+
