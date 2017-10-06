32fdde42b389b1f554b4c02719d5d4c037be9262
blackst0ne <blackst0ne.ru@gmail.com>
2017-10-06 15:34:22 +1100

    Add partners

--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -44,6 +44,34 @@
           </b-row>
         </b-col>
       </b-row>
+      <b-row class="partners">
+        <b-col>
+          <b-row class="d-flex justify-content-center">
+            <h4>Партнёры проекта</h4>
+          </b-row>
+          <b-row class="d-flex align-items-center">
+            <b-col class="text-center">
+              <img src="../assets/img/logos/mchs.png" class="img-responsive" id="logo-mchs" alt="МЧС России" />
+              <b-tooltip target="logo-mchs" title="МЧС России"></b-tooltip>
+            </b-col>
+
+            <b-col class="text-center">
+              <img src="../assets/img/logos/sakhalin-government.png" class="img-responsive" id="logo-sakhalin-government" alt="Правительство Сахалинской области" />
+              <b-tooltip target="logo-sakhalin-government" title="Правительство Сахалинской области"></b-tooltip>
+            </b-col>
+
+            <b-col class="text-center">
+              <img src="../assets/img/logos/idg.jpg" class="img-responsive" id="logo-idg" alt="ИДГ РАН" />
+              <b-tooltip target="logo-idg" title="ИДГ РАН"></b-tooltip>
+            </b-col>
+
+            <b-col class="text-center">
+              <img src="../assets/img/logos/vc.png" class="img-responsive" id="logo-vc" alt="ВЦ ДВО РАН" />
+              <b-tooltip target="logo-vc" title="ВЦ ДВО РАН"></b-tooltip>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -117,54 +145,86 @@ export default {
 
   $border: 1px $color-gray-light solid;
 
-  .events-analytics-block {
-    border-top: $border;
-    height: auto;
-    margin-top: 3%;
+  .mainpage {
+    .events-analytics-block {
+      border-top: $border;
+      height: auto;
+      margin-top: 3%;
 
-    .header {
-      padding-bottom: 3%;
-      padding-top: 3%;
-    }
-  }
-
-  .event {
-    border-left: $border;
-    border-right: $border;
-    border-top: $border;
-    padding-bottom: 2%;
-    padding-top: 2%;
-
-    &:hover {
-      background-color: $color-white-2;
-    }
-
-    &:last-child {
-      border-bottom: $border;
-    }
-
-    .magnitude {
-      color: $color-orange;
-      font-size: 180%;
-    }
-
-    .datetime {
-      color: $color-gray-light-2;
-      font-size: 85%;
-    }
-
-    .moment-tensor {
-      img {
-        max-width: 100%;
+      .header {
+        padding-bottom: 3%;
+        padding-top: 3%;
       }
     }
 
-    a {
-      color: $color-gray-dark;
-      width: 100%;
+    .partners {
+      background-color: $color-gray-light-3;
+      border: $border;
+      padding-bottom: 3%;
+      padding-top: 3%;
+
+      .row:last-child {
+        height: 250px;
+      }
+
+      .col:nth-child(3) {
+        img {
+          max-width: 90%;
+        }
+      }
+
+      h4 {
+        margin-bottom: 3%;
+      }
+
+      img {
+        filter: grayscale(100%);
+        max-width: 50%;
+
+        &:hover {
+          filter: grayscale(0%);
+        }
+      }
+    }
+
+    .event {
+      border-left: $border;
+      border-right: $border;
+      border-top: $border;
+      padding-bottom: 2%;
+      padding-top: 2%;
 
       &:hover {
-        text-decoration: none;
+        background-color: $color-white-2;
+      }
+
+      &:last-child {
+        border-bottom: $border;
+      }
+
+      .magnitude {
+        color: $color-orange;
+        font-size: 180%;
+      }
+
+      .datetime {
+        color: $color-gray-light-2;
+        font-size: 85%;
+      }
+
+      .moment-tensor {
+        img {
+          max-width: 100%;
+        }
+      }
+
+      a {
+        color: $color-gray-dark;
+        width: 100%;
+
+        &:hover {
+          text-decoration: none;
+        }
       }
     }
   }
