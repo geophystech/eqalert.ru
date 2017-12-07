a48935fa05a078d667f7ae09b4170d03c6b9e432
blackst0ne <blackst0ne.ru@gmail.com>
2017-12-08 09:53:06 +1100

    Update config

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -10,8 +10,7 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 50000)
-      // .waitForElementPresent('#app', 5000)
+      .waitForElementPresent('#app', 5000)
       // .assert.elementPresent('.hello')
       // .assert.containsText('h1', 'Welcome to Your Vue.js App')
       // .assert.elementCount('img', 1)
