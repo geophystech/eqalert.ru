5b95c3c1f37bb4561986eddc0c42fec1be048ee2
blackst0ne <blackst0ne.ru@gmail.com>
2018-01-29 10:02:35 +1100

    Update settlemenets tab

--- src/components/maps/Settlements.vue
+++ src/components/maps/Settlements.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="map" :id="mapId" />
+  <div class="map" :id="map.id" />
 </template>
 
 <script>
-  import { addEpicenter, createMap, id, convertMsk64, msk64Color } from '@/map_functions.js'
+  import { addEpicenter, createMap, id, convertMsk64, msk64Color, removeEpicenter, setView } from '@/map_functions.js'
 
   export default {
     props: ['event', 'tab'],
     data() {
       return {
         coordinates: [],
-        map: null,
-        mapId: ''
+        map: {
+          epicenter: null,
+          id: null,
+          object: null,
+          circles: []
+        }
       }
     },
     methods: {
@@ -24,7 +28,8 @@
 
           const circle = window.L.circle(this.coordinates, item.distance * 1000, { color: color, fillColor: color })
 
-          circle.addTo(this.map)
+          this.map.circles.push(circle)
+          circle.addTo(this.map.object)
           circle.bindPopup(`<div class="text-center"><strong>${value}</strong></div>`)
 
           legendData += `<i style="background: ${color}"></i>${value}<br>`
@@ -40,27 +45,47 @@
             return div
           }
 
-          legend.addTo(this.map)
+          legend.addTo(this.map.object)
         }
 
-        addEpicenter(this.map, this.coordinates)
+        this.map.epicenter = addEpicenter(this.map.object, this.coordinates)
       },
       createMap: function() {
-        this.map = createMap(this.mapId, this.coordinates)
+        this.map.object = createMap(this.map.id, this.coordinates)
       },
       fetchData: function() {
         this.$http.get(this.$root.$options.settings.api.endpointEventMsk64(this.event.id))
           .then(response => { this.addData(response.data.data) })
           .catch(error => { console.log(error) })
+      },
+      initialize: function() {
+        this.map.id = id(this.event.id, this.tab)
+        this.coordinates = [this.event.locValues.data.lat, this.event.locValues.data.lon]
+      },
+      removeData: function() {
+        // Remove map legend and MSK64 circles.
+        this.$el.querySelector('.map-legend').remove()
+        this.map.circles.forEach(circle => { this.map.object.removeLayer(circle) })
+      },
+      resetMap: function() {
+        removeEpicenter(this.map.object, this.map.epicenter)
+        this.removeData()
+        setView(this.map.object, this.coordinates)
       }
     },
     created() {
-      this.mapId = id(this.event.id, this.tab)
-      this.coordinates.push(this.event.locValues.data.lat, this.event.locValues.data.lon)
+      this.initialize()
     },
     mounted() {
       this.createMap()
       this.fetchData()
+    },
+    watch: {
+      event: function(data) {
+        this.initialize()
+        this.resetMap()
+        this.fetchData()
+      }
     }
   }
 </script>
