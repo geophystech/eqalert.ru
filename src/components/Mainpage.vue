b1beab79c92e34e475dc735b7565fb7d81d5a07e
blackst0ne <blackst0ne.ru@gmail.com>
2018-01-30 17:02:20 +1100

    Fix last events links

--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -11,14 +11,14 @@
                 <h4>Тензор момента</h4>
               </b-row>
               <b-row class="event" align-v="center" v-for="event in events" :key="event.id">
-                <router-link :to="{ name: 'Event', params: { id: event.id }}" class="d-flex align-items-center">
+                <router-link :to="{ name: 'Event', params: { id: event.hashid }}" class="d-flex align-items-center">
                   <b-col cols="2" class="magnitude text-center">{{ event.magnitude }}</b-col>
                   <b-col>
                     <div class="settlement">{{ event.nearest_settlement.distance }} км до {{ event.nearest_settlement.title }}</div>
                     <div class="datetime">{{ moment.utc(event.datetime).locale('ru').format('LL в HH:mm:ss UTC') }}, глубина {{ event.depth }} км</div>
                   </b-col>
                   <b-col cols="2" class="moment-tensor text-center">
-                    <router-link :to="{ name: 'Event', params: { id: event.id, tab: 'moment-tensor' }}">
+                    <router-link :to="{ name: 'Event', params: { id: event.hashid, tab: 'moment-tensor' }}">
                       <img :alt="event.id" :src="'https://eqalert.ru/uploads/images/moment_tensors/preview/' + event.moment_tensor.image_preview" class="img-responsive" />
                     </router-link>
                   </b-col>
