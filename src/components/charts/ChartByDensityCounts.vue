ab0ff09e2066e01a5c2510937b9bfb77a961c908
blackst0ne <blackst0ne.ru@gmail.com>
2018-08-08 11:53:27 +1100

    Fix analytic charts

--- src/components/charts/ChartByDensityCounts.vue
+++ src/components/charts/ChartByDensityCounts.vue
@@ -5,7 +5,30 @@ export default Line.extend({
   props: ['filtersParams'],
   data() {
     return {
-      chartData: this.$store.getters.chartDataset,
+      chartData: {
+        datasets: [{
+          label: '',
+          data: [],
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(51,122,183,0.4)',
+          borderColor: 'rgba(51,122,183,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(51,122,183,1)',
+          pointBackgroundColor: 'rgba(51,122,183,1)',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(51,122,183,1)',
+          pointHoverBorderColor: 'rgba(51,122,183,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10
+        }],
+        labels: []
+      },
       options: {
         scales: {
           xAxes: [
@@ -46,9 +69,6 @@ export default Line.extend({
       }
     }
   },
-  created() {
-    this.drawChart()
-  },
   methods: {
     drawChart: function(params = {}) {
       this.$http.get(this.$root.$options.settings.api.endpointAnalyticsDensityCounts, { params: params })
@@ -56,6 +76,12 @@ export default Line.extend({
           this.chartData.datasets[0].label = 'Плотностное распределение повторяемости (ML)'
           this.chartData.datasets[0].data = response.data.data.counts
           this.chartData.labels = response.data.data.magnitudes
+
+          // Ugly hack to prevent showing old data on hovering.
+          // No idea why it doesn't covered by https://github.com/apertureless/vue-chartjs/blob/master/src/BaseCharts.js#L71
+          // So ¯\_(ツ)_/¯
+          // https://github.com/geophystech/eqalert.ru/issues/258
+          if (this._chart) this._chart.destroy()
 
           this.renderChart(this.chartData, this.options)
         })
