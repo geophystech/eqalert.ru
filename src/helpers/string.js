b0d280f0b880d080450d5311a4fc4fbd080c5f9a
Arman Badalyan <armancho1@gmail.com>
2019-01-18 12:39:45 +0800

    #313 Remove string helper camelToUnderscore

--- src/helpers/string.js
+++
@@ -1,9 +0,0 @@
-
-export function camelToUnderscore(value) {
-  return value.replace(/([A-Z])/g, (a, s) => `_${s.toLowerCase()}`)
-}
-
-export function camelCase(value) {
-  return value.replace(/_([a-z])/g, (a, s) => s.toUpperCase())
-}
-
