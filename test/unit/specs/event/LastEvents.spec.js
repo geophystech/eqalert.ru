941ba43603e3f380e27a423f4c56f43b078a1776
Arman <armancho1@gmail.com>
2019-05-04 18:19:04 +0800

    #137 Events tests.

--- test/unit/specs/event/LastEvents.spec.js
+++ test/unit/specs/event/LastEvents.spec.js
@@ -3,35 +3,60 @@ import LastEvents from '@/components/event/LastEvents'
 import BootstrapVue from 'bootstrap-vue'
 import { RouterLink } from '../../utils'
 import $moment from 'moment'
+import flushPromises from 'flush-promises'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
-const $http = {
-  get: () => Promise.resolve({
-    data: {
-      data: []
-    }
-  })
-}
-
-describe('event/LastEvents.vue', () => {
-
-  const $route = {
-    query: {}
-  }
-
-  const wrapper = mount(LastEvents, {
-    mocks: { $http, $moment, $route },
+function createWrapper($http)
+{
+  return mount(LastEvents, {
+    mocks: { $http, $moment, $route: {query: {}} },
     stubs: { RouterLink },
     localVue,
     propsData: {
       events: []
     }
   })
+}
 
-  it('Check component LastEvents', () => {
-    expect(wrapper.is(LastEvents)).to.eql(true)
+const resp = {
+  data: {
+    data: [
+      {
+        nearestCity: {
+          data: {
+            ep_dis: 65043.3043832,
+            settlement: {
+              data: {
+                translation: {
+                  data: {
+                    title: 'Event settlement title'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }, {
+        nearestCity: null
+      }
+    ]
+  }
+}
+
+describe('event/LastEvents.vue', () => {
+
+  const wrapper = createWrapper({
+    get: () => Promise.resolve(resp)
+  })
+
+  const respData = resp.data.data
+
+  it('Load data', async () => {
+    flushPromises().then(() => {
+      expect(wrapper.events.length).to.equal(respData.length)
+    })
   })
 
 })
