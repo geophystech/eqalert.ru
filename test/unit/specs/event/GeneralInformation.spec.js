6e093190225e2fd7515eeec02c1cf50e4316d8f0
Arman <armancho1@gmail.com>
2019-05-04 18:06:30 +0800

    #137 Events tests.

--- test/unit/specs/event/GeneralInformation.spec.js
+++ test/unit/specs/event/GeneralInformation.spec.js
@@ -2,48 +2,34 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import GeneralInformation from '@/components/event/GeneralInformation'
 import BootstrapVue from 'bootstrap-vue'
 import $moment from 'moment'
+import $http from 'axios'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
-const eventData = {
-  processingMethod: {
-    long: null
-  },
-  locValues: {
-    data: {
-      event_datetime: new Date()
-    }
-  }
-}
-
 describe('event/GeneralInformation.vue', () => {
 
-  ([
+  const wrapper = shallowMount(GeneralInformation, {
+    propsData: {
+      event: {
+        processingMethod: {
+          long: null
+        },
+        locValues: {
+          data: {
+            event_datetime: new Date()
+          }
+        }
+      }
+    },
+    mocks: {
+      $http, $moment
+    },
+    localVue
+  })
 
-    [ 'Set data by event change', wrapper => { wrapper.vm.event = eventData } ],
-    [ 'Set data default', wrapper => {} ]
-
-  ]).forEach(conf => {
-
-    const [label, mod] = conf
-
-    describe(label, () => {
-
-      const wrapper = shallowMount(GeneralInformation, {
-        propsData: { event: eventData },
-        mocks: { $moment },
-        localVue
-      })
-
-      mod(wrapper)
-
-      it('Set data', () => {
-        expect(wrapper.is(GeneralInformation)).to.eql(true)
-      })
-
-    })
-
+  it('Check component GeneralInformation', () => {
+    expect(wrapper.is(GeneralInformation)).to.eql(true)
   })
 
 })
--- test/unit/specs/event/LDOs.spec.js
+++ test/unit/specs/event/LDOs.spec.js
@@ -1,28 +1,74 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import {mount, createLocalVue} from '@vue/test-utils'
 import LDOs from '@/components/event/LDOs'
+import flushPromises from 'flush-promises'
 import BootstrapVue from 'bootstrap-vue'
 import $moment from 'moment'
-import $http from 'axios'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
-describe('event/LDOs.vue', () => {
+const $store = {
+  getters: {
+    msk64ConfigVersion: 'msk64ConfigVersion',
+    srssDBVersion: 'srssDBVersion'
+  }
+}
 
-  const wrapper = shallowMount(LDOs, {
+function createWrapper($http)
+{
+  return mount(LDOs, {
+    mocks: { $http, $moment, $store },
+    localVue,
     propsData: {
       event: {
         id: 'ob93enBa'
       }
-    },
-    mocks: {
-      $http, $moment
-    },
-    localVue
+    }
+  })
+}
+
+const resp = {
+  data: {
+    data: [
+      {
+        parts: {
+          data: [{
+            damage: {
+              data: {
+                damage_level: 5
+              }
+            }
+          }]
+        }
+      }, {
+        parts: {
+          data: [{
+            damage: {
+              data: {
+                damage_level: 5
+              }
+            }
+          }]
+        }
+      }
+    ]
+  }
+}
+
+describe('event/LDOs.vue', () => {
+
+  const wrapper = createWrapper({
+    get: () => Promise.resolve(resp)
   })
 
-  it('Check component LDOs', () => {
-    expect(wrapper.is(LDOs)).to.eql(true)
+  const respData = resp.data.data
+
+  it('Load data', async () => {
+    flushPromises().then(() => {
+      expect(wrapper.vm.items[6].value).to.equal($store.getters.srssDBVersion)
+      expect(wrapper.vm.items[7].value).to.equal(respData.length)
+      expect(wrapper.vm.items[8].value).to.equal($store.getters.msk64ConfigVersion)
+    })
   })
 
 })
