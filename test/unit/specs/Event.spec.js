698392c28799dad15b65b1cc29c05e50c5cd62b8
Arman <armancho1@gmail.com>
2019-05-08 13:32:25 +0800

    #137 Event.

--- test/unit/specs/Event.spec.js
+++ test/unit/specs/Event.spec.js
@@ -9,12 +9,12 @@ import flushPromises from 'flush-promises'
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
-function createWrapper($http)
+function createWrapper($http, authenticated)
 {
   const $store = {
     getters: {
       user: {
-        authenticated: false
+        authenticated: authenticated
       }
     }
   }
@@ -51,7 +51,7 @@ describe('Event.vue', () => {
           expect(wrapper.vm.event.id).to.eql(EVENT_DATA.id)
         })
 
-      }],
+      }, true],
 
       ['Response Success if not user authenticated', () => Promise.resolve(resp), wrapper => {
         flushPromises().then(() => {
@@ -68,15 +68,36 @@ describe('Event.vue', () => {
         flushPromises().then(() => {
           expect(wrapper.vm.errorResponse.status).to.eql(403)
         })
+      }],
+
+      ['Magnitude Types', () => Promise.resolve(resp), wrapper => {
+
+        const magnitudeTypes = {
+          'L': [['M', 'L']],
+          'b': [['m', 'b']],
+          'B': [['m', 'B']],
+          's': [['M', 's']],
+          'S': [['M', 'S']],
+          'W': [['M', 'W']],
+          'G': [['M', 'b'], ['L', 'g']],
+          'C': [['M', 'c']],
+          'mww': [['M', 'mww']],
+          '': [['M', '']]
+        }
+
+        for (let [k, v] of Object.entries(magnitudeTypes)) {
+          expect(wrapper.vm.magnitudeType(k)).to.eql(v)
+        }
+
       }]
 
     ]).forEach(conf => {
 
-      const [label, httpResp, callBack] = conf
+      const [label, httpResp, callBack, authenticated] = conf
 
       const wrapper = createWrapper({
         get: httpResp
-      })
+      }, !!authenticated)
 
       it(label, async () => {
         callBack(wrapper)
