1faeb267df0f21cc8d5ae8944cbe28cd2dee149c
Arman <armancho1@gmail.com>
2019-05-08 09:50:12 +0800

    #137 ExportDropDown.

--- test/unit/specs/ExportDropDown.spec.js
+++ test/unit/specs/ExportDropDown.spec.js
@@ -1,7 +1,9 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import ExportDropDown from '@/components/ExportDropDown'
+import EventHeader from '@/components/event/Header'
 import flushPromises from 'flush-promises'
 import BootstrapVue from 'bootstrap-vue'
+import {EVENT_DATA} from '../utils'
 
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
@@ -20,6 +22,16 @@ function createWrapper($http)
   }
 
   return mount(ExportDropDown, {
+    parentComponent: {
+      name: 'EventHeader',
+      render(createElement, context) {
+        return createElement(EventHeader, {
+          props: {
+            event: EVENT_DATA
+          }
+        })
+      }
+    },
     mocks: { $http, $store },
     attachToDocument: true,
     localVue
@@ -43,11 +55,14 @@ describe('ExportDropDown.vue', () => {
   })
 
   it('Request xls file url', async () => {
+
     wrapper.find('.dropdown-item').trigger('click')
+
     flushPromises().then(() => {
       expect(window.location.href).to.equal(resp.response.data.data.url)
       wrapper.trigger('hide')
     })
+
   })
 
 })
