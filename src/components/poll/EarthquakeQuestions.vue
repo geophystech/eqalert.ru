84c37f1113452fa46c4b987a9d827edf97ef5446
nik.m <nik.m@investment.com.au>
2022-05-30 09:27:27 +1000

    Update question rendering from API request

--- src/components/poll/EarthquakeQuestions.vue
+++ src/components/poll/EarthquakeQuestions.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-
+    <div v-for="question in questions" :key="`question-${question.id}`">
+      <b-form-group :label="question.text[localisation]">
+        <b-form-radio
+          v-for="answer in question.answers.data"
+          :key="`answer-${answer.id}`"
+          v-model="answers[`${question.id}`]"
+          :name="`question-${question.id}`"
+          :value="answer.id"
+        >{{ answer.text[localisation] }}</b-form-radio>
+      </b-form-group>
+    </div>
   </div>
 </template>
 
@@ -11,6 +21,15 @@ export default {
     questions: {
       type: Array,
       default: []
+    },
+    localisation: {
+      type: String,
+      default: 'ru'
+    }
+  },
+  data() {
+    return {
+      answers: {}
     }
   },
   mounted() {
