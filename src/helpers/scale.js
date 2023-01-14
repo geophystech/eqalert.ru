c9e67667d0dbee74799ff887bbf928f04354ca01
nikitamakovey <nikitamakovey@yahoo.com>
2023-01-14 19:52:40 +0800

    Update ru settings

--- src/helpers/scale.js
+++ src/helpers/scale.js
@@ -7,6 +7,10 @@ export function translateScale() {
         item.innerText = item.innerText.toString().replace('km', 'км')
       } else if (item.innerText.includes('mi')) {
         item.innerText = item.innerText.toString().replace('mi', 'миль')
+      } else if (item.innerText.includes('ft')) {
+        item.innerText = item.innerText.toString().replace('ft', 'футов')
+      } else if (item.innerText.includes('m')) {
+        item.innerText = item.innerText.toString().replace('m', 'м')
       }
     })
   }, 100)
