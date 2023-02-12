2893f0634a8453ecf10f227e75b40a8f0a7e9e8b
nikitamakovey <nikitamakovey@yahoo.com>
2023-02-12 11:10:18 +0800

    Fix agency and error values

--- nuxt2/helpers/event.js
+++ nuxt2/helpers/event.js
@@ -2,7 +2,7 @@ export function agency(agency, long = true) {
   switch (agency) {
     case 'SAK': return long === false ? 'GPT' : 'GEOPHYSTECH LLC'
     case 'USGS': return long === false ? 'USGS' : 'USGS Earthquake Hazards Program'
-    default: return 'Unknown'
+    default: return agency
   }
 }
 
@@ -10,7 +10,7 @@ export function agencyDescription(agency) {
   switch (agency) {
     case 'SAK': return 'GEOPHYSTECH LLC'
     case 'USGS': return 'USGS Earthquake Hazards Program'
-    default: return 'Unknown'
+    default: return agency
   }
 }
 
